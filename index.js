476b2a6edd668fb2b6022e031ce307a093557bb7
alan <alan606@gmail.com>
2018-06-01 23:23:58 -0400

    change

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
 const pug = require('pug');
+var {Book} = require('./models/book')
 
 //const {mongoose} = require('./db/mongo');
 app.set('view engine','pug');
-app.post('/',(req,res)=>{
-
-
-
-
+app.post('/book',(req,res)=>{
+    var book = new Book({
+        text: req.body.text,
+        ISBN: req.body.ISBN,
+        OnStock:req.body.OnStock
+    });
+    book.save().then((doc)=>{
+        res.send(doc);
+    },(e)=>{
+        res.status(400).send(e);
+        })
 });
+
+
+
+
  app.get('/',(req,res)=> {
      res.render("welcome");
      });
